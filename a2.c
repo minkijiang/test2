ab13acb71e716afcbd4cdf455af76c0f75427c02
minkijiang <jiangminki0@gmail.com>
2025-03-05 23:13:43 -0500

    Add files via upload

--- a2.c
+++ a2.c
@@ -830,7 +830,7 @@ int main() {
 		processes[0] = getProcess(pid);
 		processes[1] = getProcess(pid2);
 
-		//displayProcessFD(processes, 2);
+		displayProcessFD(processes, 2);
 		//displaySystemWide(processes, 2);
 		//displayVnode(processes, 2);
 		//displayComposite(processes, 2);
