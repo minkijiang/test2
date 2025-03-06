c36bbdf7049bb84ce8a7f123b83fc429787f13b7
minkijiang <jiangminki0@gmail.com>
2025-03-05 19:35:21 -0500

    Add files via upload

--- a2.c
+++ a2.c
@@ -773,12 +773,17 @@ int main() {
 
 	if (pid != 0) {
 		PROCESS* process = getProcess(pid);
+
+		/*
 		displayProcessFD(&process, 1);
 		displaySystemWide(&process, 1);
 		displayVnode(&process, 1);
 		displayComposite(&process, 1);
 		writeCompositeTXT(&process, 1);
 		writeCompositeBIN(&process, 1);
+		*/
+
+		getDistinctFiles(process);
 
 	
 
